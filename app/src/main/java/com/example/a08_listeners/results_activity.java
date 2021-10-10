5f2fd38b7b78075b4e1df878d424f3743496684c
ns6991 <ns6991@bs.amalnet.k12.il>
2021-10-10 19:50:20 +0300

    me happy

--- app/src/main/java/com/example/a08_listeners/results_activity.java
+++ app/src/main/java/com/example/a08_listeners/results_activity.java
@@ -28,8 +28,8 @@ public class results_activity extends AppCompatActivity {
         player1 = (TextView)findViewById(R.id.player1);
         player2 = (TextView)findViewById(R.id.player2);
         winner = (TextView)findViewById(R.id.winner);
-        player1.setText("player 1:" + s + "points");
-        player2.setText("player 2:" + l + "points");
+        player1.setText("player 1: " + s + " points");
+        player2.setText("player 2: " + l + " points");
         if(l<s) winner.setText("player 1 is the winner!");
         else if(l>s) winner.setText("player 2 is the winner!");
         else winner.setText("DRAW!");
